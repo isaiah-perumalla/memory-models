0e987652bfbd96fda2b11449d3a2d1d6fa167eac
isaiah-p <0x7bb@protonmail.com>
2022-09-28 19:40:45 +0100

    added compile output

--- x86-java/src/x86/mem/x86Test.java
+++ x86-java/src/x86/mem/x86Test.java
@@ -24,30 +24,29 @@ public class x86Test {
     }
 
 
-    static   long thread0Val, thread1Val = 0;
+    static long x, y = 0;
     static   long r0, r1;
-    static volatile long dummy;
-    static void thread0()
+
+    static void core0()
     {
-        thread0Val = 1;
-        UNSAFE.fullFence();
-        r1 = thread1Val;
+        x = 1;
+        r1 = y;
 
     }
 
-    static void thread1 ()
+    static void core1()
     {
-        thread1Val = 1;
-        r0 = thread0Val;
+        y = 1;
+        r0 = x;
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         long count = 0;
         while (true) {
-            Thread t0 = new Thread(x86Test::thread0);
-            Thread t1 = new Thread(x86Test::thread1);
-            thread0Val = thread1Val = 0;
+            Thread t0 = new Thread(x86Test::core0);
+            Thread t1 = new Thread(x86Test::core1);
+            x = y = 0;
             t0.start();
             t1.start();
 
@@ -56,11 +55,10 @@ public class x86Test {
             count++;
 
             if (r0 == 0 && r1 == 0) {
-                System.out.printf("(r0=%d, r1=%d, after %d runs)\n", r0, r1, count);
+                System.err.printf("(r0=%d, r1=%d, after %d runs)\n", r0, r1, count);
                 break;
             }
 
         }
-
     }
 }
